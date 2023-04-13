80f12e303caff37462cd0af97a08c73427eaf32d
shridhar7393 <shreeheg@gmail.com>
2023-04-14 02:19:39 +0530

    junit test scenario

--- MakkajaiDevChallengeTaskShridhar/src/test/SalesTaxUnitTest.java
+++ MakkajaiDevChallengeTaskShridhar/src/test/SalesTaxUnitTest.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,17 +22,13 @@ import salestax.SalesTaxCalculator;
  *
  */
 public class SalesTaxUnitTest {
-	String inputString = "1 imported bottle of perfume at 47.50";
+	
 	String in1 = "src/test1.txt";
 	String[] out1 = {"1 book: 12.49","1 music CD: 16.49","1 chocolate bar: 0.85"};
 
+	
 
-	String in2 = "src/test2.txt";
-	String[] out2 = {"1 imported box of chocolates: 10.50","1 imported bottle of perfume: 54.65"};
-
-	String in3 = "src/test3.txt";
-	String[] out3 = {"1 imported bottle of perfume: 32.19","1 bottle of perfume: 20.89","1 packet of headache pills: 9.75","1 imported box of chocolates: 11.85"};
-
+	
 	public ArrayList<Item> createListFromInputFile(String fileName){
 		ArrayList<Item> itemsList = new ArrayList<Item>();
 		File file = new File(fileName);
@@ -81,20 +76,20 @@ public class SalesTaxUnitTest {
 	}
 
 	@Test
-	public void testWhetherInputStringIsConvertedToListOfItems() {
+	public void testPositiveWhetherInputStringIsConvertedToListOfItems() {
+		String inputString = "1 imported bottle of perfume at 47.50";
 		ArrayList<Item> aList = createListFromInputString(inputString);
-		SalesTaxCalculator sc = new SalesTaxCalculator();
 		for(Item item: aList) {
 			assertTrue(item instanceof Item);// string is read and converted to Item object
 			assertEquals(1, item.getQuntity());
-			assertEquals("47.50", sc.roundUpToTwoDecimals(item.getPrice()).toString());
+			assertEquals(47.50, item.getPrice(), 0.00);
 			assertEquals(true, item.isImported());
 			assertEquals(ItemType.OTHER, item.getItemType());
 		}
 	}
 
 	@Test
-	public void inputFileIsConvertedToItemsListwithValues() {
+	public void testPositiveWhetherInputFileIsConvertedToItemsListwithNotNull() {
 		ArrayList<Item> aList = createListFromInputFile(in1);
 		for(Item item: aList) {
 			assertTrue(item instanceof Item);
@@ -106,54 +101,58 @@ public class SalesTaxUnitTest {
 	}
 
 	@Test
-	public void testOutputFromInput1() {
-		SalesTaxCalculator stc1 = new SalesTaxCalculator();
+	public void testPositiveOutputFromInput1() {
 		Receipt rc = new Receipt();
 		rc.generateReceipt(createListFromInputFile(in1));
 		ArrayList<Item> items = (ArrayList<Item>) rc.getItemsList();
 		int i = 0;
 		for (Item item : items) {
-
 			assertEquals(out1[i++], item.toString());
-			assertEquals("1.50", stc1.roundUpToTwoDecimals(rc.getTotalSalesTax()).toString());
-			assertEquals("29.83", stc1.roundUpToTwoDecimals(rc.getTotalCost()).toString());
-
 		}
+		assertEquals(1.50, rc.getTotalSalesTax(), 0.2);
+		assertEquals(29.83, rc.getTotalCost(), 0.2);
 
 	}
 
-		@Test
-		public void testOutputFromInput2() {
-			SalesTaxCalculator stc1 = new SalesTaxCalculator();
-			Receipt rc = new Receipt();
-			rc.generateReceipt(createListFromInputFile(in2));
-			ArrayList<Item> items = (ArrayList<Item>) rc.getItemsList();
-			int i = 0;
-			for (Item item : items) {
+	@Test
+	public void testPositiveOutputFromInput2() {
 
-				assertEquals(out2[i++], item.toString());
-				assertEquals("7.65", stc1.roundUpToTwoDecimals(rc.getTotalSalesTax()).toString());
-				assertEquals("65.15", stc1.roundUpToTwoDecimals(rc.getTotalCost()).toString());
-
-			}
-	
+		String in2 = "src/test2.txt";
+		String[] out2 = {"1 imported box of chocolates: 10.50","1 imported bottle of perfume: 54.65"};
+		Receipt rc = new Receipt();
+		rc.generateReceipt(createListFromInputFile(in2));
+		ArrayList<Item> items = (ArrayList<Item>) rc.getItemsList();
+		int i = 0;
+		for (Item item : items) {
+			assertEquals(out2[i++], item.toString());
 		}
+		assertEquals(7.65, rc.getTotalSalesTax(), 0.2);
+		assertEquals(65.15, rc.getTotalCost(), 0.2);
 
-		@Test
-		public void testOutputFromInput13() {
-			SalesTaxCalculator stc1 = new SalesTaxCalculator();
-			Receipt rc = new Receipt();
-			rc.generateReceipt(createListFromInputFile(in3));
-			ArrayList<Item> items = (ArrayList<Item>) rc.getItemsList();
-			int i = 0;
-			for (Item item : items) {
+	}
 
-				assertEquals(out3[i++], item.toString());
-				assertEquals("6.70", stc1.roundUpToTwoDecimals(rc.getTotalSalesTax()).toString());
-				assertEquals("74.68", stc1.roundUpToTwoDecimals(rc.getTotalCost()).toString());
+	@Test
+	public void testPositiveOutputFromInput3() {
+		String in3 = "src/test3.txt";
+		String[] out3 = {"1 imported bottle of perfume: 32.19","1 bottle of perfume: 20.89","1 packet of headache pills: 9.75","1 imported box of chocolates: 11.85"};
 
-			}
-	
+		Receipt rc = new Receipt();
+		rc.generateReceipt(createListFromInputFile(in3));
+		ArrayList<Item> items = (ArrayList<Item>) rc.getItemsList();
+		int i = 0;
+		for (Item item : items) {
+			assertEquals(out3[i++], item.toString());
 		}
+		assertEquals(6.70, rc.getTotalSalesTax(), 0.2);
+		assertEquals(74.68, rc.getTotalCost(), 0.2);
+
+	}
+
+	@Test
+	public void testPositiveOutputOfCalculatePriceWithTaxFunctionFromSalesTaxCalculator() {
+		SalesTaxCalculator stc1 = new SalesTaxCalculator();
+		Item testItem = new Item("bottle of perfume", 364.00,1,ItemType.OTHER,true);
+		assertEquals(418.60, stc1.calculatePriceWithTax(testItem), 0.2);
+	}
 
 }
