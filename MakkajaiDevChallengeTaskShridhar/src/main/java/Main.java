fa56458d79b93f8b4eef826b322a39bd4611846f
shridhar7393 <shreeheg@gmail.com>
2023-04-19 03:13:18 +0530

    jar updated

--- MakkajaiDevChallengeTaskShridhar/src/main/java/Main.java
+++ MakkajaiDevChallengeTaskShridhar/src/main/java/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     Receipt receipt = new Receipt();
-//    receipt.generateReceiptFromFileInput(args[0]);
+    receipt.generateReceiptFromFileInput(args[0]);
     
     System.out.println("Output");
     for (Item item : receipt.getItemsList()) {
-      System.out.printf("%s: %n", item.toString());
+      System.out.printf("%s %n", item.toString());
     }
 
     System.out.printf("Sales Taxes: %s%n", receipt.getTotalSalesTax());
